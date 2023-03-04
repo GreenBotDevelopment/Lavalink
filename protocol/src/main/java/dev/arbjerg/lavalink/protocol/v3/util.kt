18528041fd6048eb271dabd9ada6614ed4a0fa42
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2023-03-04 11:08:47 +0100

    fix

--- protocol/src/main/java/dev/arbjerg/lavalink/protocol/v3/util.kt
+++ protocol/src/main/java/dev/arbjerg/lavalink/protocol/v3/util.kt
@@ -11,7 +11,7 @@ import java.io.ByteArrayOutputStream
 fun decodeTrack(audioPlayerManager: AudioPlayerManager, message: String): AudioTrack {
     val bais = ByteArrayInputStream(Base64.decodeBase64(message))
     return audioPlayerManager.decodeTrack(MessageInput(bais)).decodedTrack
-        ?: throw IllegalStateException("Failed to decode track due to a mismatching version or missing source manager")
+        ?: log.info("Failed to decode track due to a mismatching version or missing source manager")
 }
 
 fun encodeTrack(audioPlayerManager: AudioPlayerManager, track: AudioTrack): String {
