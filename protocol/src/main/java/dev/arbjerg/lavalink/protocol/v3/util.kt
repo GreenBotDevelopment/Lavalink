dc4138ce812067b7ed11e6bad07df0ecc38af9ec
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2023-03-05 08:19:20 +0100

    fe

--- protocol/src/main/java/dev/arbjerg/lavalink/protocol/v3/util.kt
+++ protocol/src/main/java/dev/arbjerg/lavalink/protocol/v3/util.kt
@@ -14,9 +14,10 @@ fun decodeTrack(audioPlayerManager: AudioPlayerManager, message: String): AudioT
     val bais = ByteArrayInputStream(Base64.decodeBase64(message))
     return try {
         audioPlayerManager.decodeTrack(MessageInput(bais)).decodedTrack
-            ?: throw Exception("Failed to decode track")
+            ?: throw TrackDecodingException("Failed to decode track")
     } catch (e: IllegalStateException) {
         // Handle the exception here
+        throw TrackDecodingException("An error occurred while decoding track: ${e.message}")
     }
 }
 fun encodeTrack(audioPlayerManager: AudioPlayerManager, track: AudioTrack): String {
