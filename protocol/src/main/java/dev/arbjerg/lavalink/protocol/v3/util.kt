114912877b462cfbcc4eaa34181e50d98f090b7b
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2023-03-05 08:10:57 +0100

    fix

--- protocol/src/main/java/dev/arbjerg/lavalink/protocol/v3/util.kt
+++ protocol/src/main/java/dev/arbjerg/lavalink/protocol/v3/util.kt
@@ -8,13 +8,15 @@ import org.apache.commons.codec.binary.Base64
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 
-fun decodeTrack(audioPlayerManager: AudioPlayerManager, message: String): AudioTrack? {
+class TrackDecodingException(message: String) : Exception(message)
+
+fun decodeTrack(audioPlayerManager: AudioPlayerManager, message: String): AudioTrack {
     val bais = ByteArrayInputStream(Base64.decodeBase64(message))
     return try {
         audioPlayerManager.decodeTrack(MessageInput(bais)).decodedTrack
     } catch (e: IllegalStateException) {
         // Handle the exception here
-        null
+       
     }
 }
 fun encodeTrack(audioPlayerManager: AudioPlayerManager, track: AudioTrack): String {
