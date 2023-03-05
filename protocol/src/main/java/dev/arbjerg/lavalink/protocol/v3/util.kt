20c668d503dc9049f329f5fab09d10224b2daa08
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2023-03-05 08:34:15 +0100

    fix

--- protocol/src/main/java/dev/arbjerg/lavalink/protocol/v3/util.kt
+++ protocol/src/main/java/dev/arbjerg/lavalink/protocol/v3/util.kt
@@ -9,9 +9,6 @@ import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import org.slf4j.LoggerFactory
 
-
-class TrackDecodingException(message: String) : Exception(message)
-
 fun decodeTrack(audioPlayerManager: AudioPlayerManager, message: String): AudioTrack? {
     val bais = ByteArrayInputStream(Base64.decodeBase64(message))
     val track = audioPlayerManager.decodeTrack(MessageInput(bais))
